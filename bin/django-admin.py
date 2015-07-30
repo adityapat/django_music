762d95c9f873a62a7d9104926f8857c78f60307d
kartikey406 <kartikey204@gmail.com>
2015-07-30 11:45:28 -0700

    intial

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/root/Desktop/last_django/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.8.3','django-admin.py'
+__requires__ = 'Django==1.8.3'
+import pkg_resources
+pkg_resources.run_script('Django==1.8.3', 'django-admin.py')
